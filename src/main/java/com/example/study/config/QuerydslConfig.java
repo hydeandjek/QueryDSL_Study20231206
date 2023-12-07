3523ec543c635c883bdfaac0d8de168f424de581
hydeandjek <hydeandjek@naver.com>
2023-12-07 09:37:47 +0900

    ###

---
+++ src/main/java/com/example/study/config/QuerydslConfig.java
@@ -0,0 +1,24 @@
+package com.example.study.config;
+
+// QueryDSL 문법을 사용하기 위한 필수 객체인 JPAQueryFactory 의 빈등록을 위한 클래스
+// 나중에 여러 개의 repository에서 QureyDSL 을 사용하기 위한 빈 등록
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Configuration
+public class QuerydslConfig {
+
+    @PersistenceContext // JPA 라이브러리를 사용한다면 객체 주입 가능
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(){
+        return new JPAQueryFactory(entityManager);
+
+    }
+}
